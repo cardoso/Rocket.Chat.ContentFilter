f65d6570d4fcea7acb51d011f73ec700c1f3982e
cardoso <matheus@cardo.so>
2018-09-08 00:13:04 -0300

    🎉 initial commit

---
+++ ContentFilterApp.ts
@@ -0,0 +1,71 @@
+import {
+    IConfigurationExtend,
+    IEnvironmentRead,
+    IHttp,
+    ILogger,
+    IPersistence,
+    IRead,
+} from '@rocket.chat/apps-engine/definition/accessors';
+import { App } from '@rocket.chat/apps-engine/definition/App';
+import { IMessage, IPreMessageSentPrevent } from '@rocket.chat/apps-engine/definition/messages';
+import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+import { SettingType } from '@rocket.chat/apps-engine/definition/settings';
+
+export class ContentFilterApp extends App implements IPreMessageSentPrevent {
+    constructor(info: IAppInfo, logger: ILogger) {
+        super(info, logger);
+    }
+
+    public async checkPreMessageSentPrevent(message: IMessage, read: IRead, http: IHttp): Promise<boolean> {
+        return true;
+    }
+
+    public async executePreMessageSentPrevent(message: IMessage, read: IRead, http: IHttp, persistence: IPersistence): Promise<boolean> {
+        const regexes = (await read.getEnvironmentReader().getSettings().getValueById('Content_Filter_Regular_Expressions'))
+                                   .replace(/\r\n/g, '\r').replace(/\n/g, '\r').split(/\r/).filter((r: string) => r.length > 0);
+
+        if (regexes.length === 0) {
+            return false;
+        }
+
+        const fullContent = (message.text || '') + (message.avatarUrl || '') + (message.alias || '') + (message.emoji || '')
+                            + (message.attachments || []).reduce((total, a) => {
+                                return total + (a.audioUrl || '') + (a.description || '') + (a.imageUrl || '') + (a.text || '')
+                                       + (a.thumbnailUrl || '') + (a.timestampLink || '') + ((a.title || {}).link || '')
+                                       + ((a.title || {}).value || '') + (a.videoUrl || '')
+                                       + (a.fields || []).reduce((t, f) => {
+                                           return t + (f.title || '') + (f.value || '');
+                                       }, '');
+                            }, '');
+
+        this.getLogger().log(fullContent);
+
+        for (const r of regexes) {
+            if (RegExp(r).exec(fullContent)) {
+                read.getNotifier().notifyUser(message.sender, {
+                    room: message.room,
+                    sender: message.sender,
+                    text: 'Your message has been blocked by *Content Filter*',
+                    alias: 'Content Filter',
+                    emoji: ':no_entry:',
+                });
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected async extendConfiguration(configuration: IConfigurationExtend, environmentRead: IEnvironmentRead): Promise<void> {
+        await configuration.settings.provideSetting({
+            id: 'Content_Filter_Regular_Expressions',
+            type: SettingType.CODE,
+            packageValue: '',
+            required: true,
+            public: false,
+            i18nLabel: 'Content_Filter_Regular_Expressions',
+            i18nDescription: 'Content_Filter_Regular_Expressions_Description',
+        });
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Rocket.Chat.ContentFilter
+⛔️ Content Filter App for Rocket.Chat - keep scammers and undesirable content out of your server
+
+![](docs/screenshot_02.png)
+
+## Usage
+### Regular Expressions
+This app uses a list of regular expressions to block content sent from users, the searching is done not only in the message's text but also in attachment titles, fields, aliases and anything else that can be displayed as text or link.
+
+You can define the list of regular expressions in the App's settings.
+
+![](docs/screenshot_01.png)
+
+## Planned features
+See the [issues list](https://github.com/cardoso/Rocket.Chat.ContentFilter/issues)
